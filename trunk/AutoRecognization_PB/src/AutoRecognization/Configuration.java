fd13bf54600ba73a416157e3991f45f8474e9286
dodatdainc <dodatdainc@b8f89b5c-c533-11de-a4c1-8109ed5598a0>
2009-11-19 01:54:48 +0000

    Commit library

--- trunk/AutoRecognization_PB/src/AutoRecognization/Configuration.java
+++ trunk/AutoRecognization_PB/src/AutoRecognization/Configuration.java
@@ -121,4 +121,25 @@ public class Configuration {
             return ret;
         }
     }
+
+     public String readTempleteField2() {
+        String ret = "";
+        try {
+            BufferedReader input = null;
+            StringBuilder contents = new StringBuilder();
+            File file = new File("Field2Templete.txt");
+            if (file.exists()) {
+                input = new BufferedReader(new FileReader(file));
+                while ((ret = input.readLine()) != null) {
+                    contents.append(ret);
+                    contents.append(System.getProperty("line.separator"));
+                }
+                ret = contents.toString();
+                input.close();
+            }
+        } catch (IOException ex) {
+        } finally {
+            return ret;
+        }
+    }
 }
