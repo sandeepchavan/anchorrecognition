f2bf3eafd1db8f780d8c7cc698740c67a27a9ef0
dodatdainc <dodatdainc@b8f89b5c-c533-11de-a4c1-8109ed5598a0>
2009-11-25 07:30:24 +0000

    Commit library

--- StripeReader/src/StripeReader/core/Reader.java
+++ StripeReader/src/StripeReader/core/Reader.java
@@ -18,7 +18,7 @@ public interface Reader {
      * hints, each possibly associated to some data, which may help the implementation decode.
      *
      * @param image image of barcode to decode
-     * @param hints passed as a {@link java.util.Hashtable} from {@link com.google.zxing.DecodeHintType}
+     * @param hints passed as a {@link java.util.Hashtable}
      * to arbitrary data. The
      * meaning of the data depends upon the hint type. The implementation may or may not do
      * anything with these hints.
